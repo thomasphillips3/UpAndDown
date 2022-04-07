fd035f65c0ed7023a7145a602d4d22e40ea04f99
Thomas Phillips <thomasphillips3@gmail.com>
2022-04-06 23:47:01 -0700

    view binding and +/- logic

--- app/src/main/java/com/thomasphillips3/upanddown/MainActivity.kt
+++ app/src/main/java/com/thomasphillips3/upanddown/MainActivity.kt
@@ -2,10 +2,31 @@ package com.thomasphillips3.upanddown
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thomasphillips3.upanddown.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        var counter = 0
+
+        binding.numberText.text = counter.toString()
+
+        binding.minusButton.setOnClickListener{
+            counter--
+            binding.numberText.text = counter.toString()
+        }
+
+        binding.plusButton.setOnClickListener{
+            counter++
+            binding.numberText.text = counter.toString()
+        }
+
+        binding.clearButton.setOnClickListener {
+            counter = 0
+            binding.numberText.text = counter.toString()
+        }
+
+        setContentView(binding.root)
     }
 }
